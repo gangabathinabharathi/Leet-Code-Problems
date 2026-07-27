53dfc32336047d7499f6f3553208ab1d03dd98e5
gangabathinabharathi <bg9137235@gmail.com>
2026-07-27 12:57:05 +0530

    Create 65.Valid Number.java

---
+++ 65.Valid Number.java
@@ -0,0 +1,5 @@
+var checkIfInstanceOf = function(obj, classFunction) {
+    if(obj === null || obj === undefined || typeof classFunction !== 'function')
+        return false;
+    return Object(obj) instanceof classFunction;
+};
